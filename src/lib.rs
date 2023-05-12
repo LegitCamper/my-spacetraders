0f573932c94b439364c03467006959604063a2c4
sawyer bristol <sawyerbristol@gmail.com>
2023-05-12 11:20:47 -0600

    fixing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 use std::sync::Arc;
-use tokio::{sync::broadcast, task};
+use tokio::{
+    sync::broadcast,
+    task,
+    time::{sleep, Duration},
+};
 
 mod interface;
-pub use interface::{Credentials, SpaceTraders};
+pub use interface::{AgentInfoL0, Credentials, SpaceTraders};
 
 // Creates instance of iterface
 pub struct InterfaceHandler {
@@ -24,6 +28,7 @@ impl InterfaceHandler {
         let mut receiver = self.sender.subscribe();
         task::spawn(async move {
             loop {
+                sleep(Duration::from_millis(500)).await; // only 2 requests per second
                 let i = receiver.recv().await.unwrap();
                 if i.receiver == BroadcastReceiver::Interface {
                     println!("{:?}", i.message);
@@ -37,10 +42,17 @@ impl InterfaceHandler {
 #[derive(Debug)]
 pub struct Broadcast {
     pub receiver: BroadcastReceiver,
-    pub message: Option<String>,
+    pub message: BroadcastMessage,
 }
 #[derive(Debug, Eq, PartialEq)]
 pub enum BroadcastReceiver {
     Caller,
     Interface,
+}
+#[derive(Debug)]
+pub enum BroadcastMessage {
+    String(String),
+    AgentInfo,
+    AgentInfoResponse(AgentInfoL0),
+    WaypointDetails(String, String),
 }
