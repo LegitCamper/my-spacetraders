7154af6a4ca00d2dbcee4686ee35d73c0b62b704
sawyer bristol <sawyerbristol@gmail.com>
2023-05-12 11:21:49 -0600

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
-// use my_spacetraders::interface::{
-//     parse_waypoint, BuyShip, Coordinates, Credentials, Method, SpaceTraders,
-// };
+use my_spacetraders::interface::{
+    parse_waypoint, BuyShip, Coordinates, Credentials, Method, SpaceTraders,
+};
 
-use my_spacetraders::{Broadcast, BroadcastReceiver, Credentials, InterfaceHandler};
+// use my_spacetraders::{Broadcast, BroadcastReceiver, Credentials, InterfaceHandler};
 
 use std::sync::Arc;
+use tokio::sync::broadcast;
 
-#[tokio::main]
-async fn main() {
+async fn create_interface() -> broadcast::Sender<Arc<Broadcast>> {
     let credentials = Credentials::new(
         "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiTEVHSVRDQU1QRVIiLCJpYXQiOjE2ODM2NTE0MDcsInN1YiI6ImFnZW50LXRva2VuIn0.fB1FkKfy57TZZCy9DNBm24aIRrYwxM1JbuHWd7sA6qdDYVbkCVwVliEP27nzkU3fcY2XbL0InUxsx-biV9Ux9fJcPUzyOEj92X_I8ZzxNTIrj4J7g7Zyp1Hbpr9056BmrshkrzeX65PspVZvSPmXLsntFiZsF1ncmlgnjePElfUcUdY2wY12xgMjne6sJXueDGNzEXSrkARDexCeogfwYXo3RxfVtwtx6mxI3z7hPaa80c8cHhvTteKLJ3eioIWhs8Yv4xnaQIwakLQcvyrVNmFPYl5mjOMt9rBzTP5cGpcko7AXlzNfpXdFK6O_3fU1PgwhutuFTZPKFCMDLetK2aEwp9f5Rg_KHaCglUMkkjsvJw1AGYcSKVP1eLB5KTxRw9UnNwAwi4ocGKNm9AlaJionFUIZXpRCOs9T6pnntV5IdGuNlZ5JR40buaBA6I3g5Lqa_Sg7g1NVF9Wt3ZgrgpKed4l7frd6mupq1JE2sm0-XVYH7f6H4Cx3Em2uzNxe"
     );
-
     let interface_handler = InterfaceHandler::new(credentials);
     interface_handler.spawn().await;
+    interface_handler.sender
+}
 
-    let message = Broadcast {
-        receiver: BroadcastReceiver::Interface,
-        message: Some("hello from main!".to_string()),
-    };
+fn main_algo() {
+    complete_contracts();
+}
 
-    interface_handler.sender.send(message.into()).unwrap();
+fn complete_contracts() {}
 
-    // let space_traders = SpaceTraders::new(credentials);
+#[tokio::main]
+async fn main() {
+    let interface_sender = create_interface().await;
+
+    // let message = Broadcast {
+    //     receiver: BroadcastReceiver::Interface,
+    //     message: Some("hello from main!".to_string()),
+    // };
+
+    interface_sender.send(message.into()).unwrap();
 
     // prints agent info
     // let agent_info = space_traders.agent_details().await;
